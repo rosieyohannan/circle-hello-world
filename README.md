d2de2d85018ceaac7ccc2f6f9d3ee159d74d1223
rosie yohannan <rosie@circleci.com>
2026-02-25 23:12:27 +0000

    simple test

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CircleCI hello world example
+
+Test branch for rerun failed tests screenshots
---
+++ test_sample.py
@@ -0,0 +1,9 @@
+def test_passing_one():
+    assert 1 + 1 == 2
+
+def test_passing_two():
+    assert 2 + 2 == 4
+
+def test_will_fail():
+    # Change this to make it fail for your screenshot
+    assert 1 + 1 == 2  # Change to == 3 to make it fail
